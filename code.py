b20be5c3dfc980613c6474932a029f9d67728018
HamsterBoomer <76519196+HamsterBoomer@users.noreply.github.com>
2026-02-17 15:36:15 +0100

    Add image background removal functionality

---
+++ code.py
@@ -0,0 +1,13 @@
+# Code
+pip install rembg pillow
+
+from rembg import remove
+from PIL import Image
+
+input_path = 'pk.png'
+output_path = 'pkjsp.png'
+
+inp = Image.open(input_path)
+output = remove(inp)
+output.save(output_path)
+Image.open(output_path).show()  # If you want to display it
